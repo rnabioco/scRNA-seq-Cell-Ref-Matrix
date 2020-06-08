7f11069c6c98fc916c8fbdfece0c1fe05f5c52d7
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-06-08 12:07:51 -0600

    Changed to using whole cell data

--- R/DiabetesFibroblastGSE148946Nuclei.r
+++ R/DiabetesFibroblastGSE148946Nuclei.r
@@ -4,16 +4,16 @@ library(patchwork)
 library(clustifyr)
 library(tidyverse)
 
-mat_FibroblastNucleiDiabetes <- read_csv("ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE148nnn/GSE148946/suppl/GSE148946_nuclei_normdata.csv.gz")
+mat_FibroblastNucleiDiabetes <- read_csv("ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE148nnn/GSE148946/suppl/GSE148946_wholecell_normdata.csv.gz")
 mat_FibroblastNucleiDiabetes <- mat_FibroblastNucleiDiabetes %>%
   as.data.frame() %>%
-  column_to_rownames('X1') %>%
+  column_to_rownames('gene') %>%
   as.matrix() %>%
   t()
 mat_FibroblastNucleiDiabetes[1:5, 1:5]
 
-meta_FibroblastNucleiDiabetes <- read_csv("ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE148nnn/GSE148946/suppl/GSE148946_nuclei_metadata.csv.gz")
-sum(colnames(mat_FibroblastNucleiDiabetes) %in% meta_FibroblastNucleiDiabetes$celltype)
+meta_FibroblastNucleiDiabetes <- read_csv("ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE148nnn/GSE148946/suppl/GSE148946_wholecell_metadata.csv.gz")
+sum(colnames(mat_FibroblastNucleiDiabetes) %in% meta_FibroblastNucleiDiabetes$orig.ident)
 ncol(mat_FibroblastNucleiDiabetes)
 
 FibroblastNucleiDiabetes <- CreateSeuratObject(counts = mat_FibroblastNucleiDiabetes, project = "FibroblastNucleiDiabetes", min.cells = 3, min.features = 200)
@@ -26,3 +26,14 @@ plot1 <- FeatureScatter(FibroblastNucleiDiabetes, feature1 = "nCount_RNA", featu
 plot2 <- FeatureScatter(FibroblastNucleiDiabetes, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
 plot1 + plot2
 FibroblastNucleiDiabetes <- subset(FibroblastNucleiDiabetes, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
+
+#Variable Features
+FibroblastNucleiDiabetes <- FindVariableFeatures(FibroblastNucleiDiabetes, selection.method = "vst", nfeatures = 2000)
+top10 <- head(VariableFeatures(FibroblastNucleiDiabetes), 10)
+plot1 <- VariableFeatures(FibroblastNucleiDiabetes)
+plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+plot1 + plot2
+
+#Linear dimension reduction 
+all.genes <- rownames(FibroblastNucleiDiabetes)
+FibroblastNucleiDiabetes <- ScaleData(FibroblastNucleiDiabetes, features = all.genes)
