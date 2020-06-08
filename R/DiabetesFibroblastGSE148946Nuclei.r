c6b39609349aebb726ccc4b99d0beb18dff72dd5
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-06-08 09:01:01 -0600

    Added nCountsRNA and nFeatures_RNA

--- R/DiabetesFibroblastGSE148946Nuclei.r
+++ R/DiabetesFibroblastGSE148946Nuclei.r
@@ -18,3 +18,11 @@ ncol(mat_FibroblastNucleiDiabetes)
 
 FibroblastNucleiDiabetes <- CreateSeuratObject(counts = mat_FibroblastNucleiDiabetes, project = "FibroblastNucleiDiabetes", min.cells = 3, min.features = 200)
 FibroblastNucleiDiabetes
+FibroblastNucleiDiabetes@assays$RNA@data <- FibroblastNucleiDiabetes@assays$RNA@counts
+FibroblastNucleiDiabetes[["percent.mt"]] <- PercentageFeatureSet(FibroblastNucleiDiabetes, pattern = "^MT-")
+head(FibroblastNucleiDiabetes@meta.data, 5)
+VlnPlot(FibroblastNucleiDiabetes, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+plot1 <- FeatureScatter(FibroblastNucleiDiabetes, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot2 <- FeatureScatter(FibroblastNucleiDiabetes, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+plot1 + plot2
+FibroblastNucleiDiabetes <- subset(FibroblastNucleiDiabetes, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
