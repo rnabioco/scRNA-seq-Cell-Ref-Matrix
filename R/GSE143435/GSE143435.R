f948068782232abc947d2a5415c0661df3c6ed53
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-06-09 08:59:35 -0600

    Added new csvs and modify matrices

--- R/GSE143435/GSE143435.R
+++ R/GSE143435/GSE143435.R
@@ -4,7 +4,7 @@ library(patchwork)
 library(clustifyr)
 library(tidyverse)
 
-D0_FACSatlas <- read_csv("~/Reference-Matrix-Generation/data/GSE143435/GSE143435.csv")
+D0_FACSatlas <- read_tsv("ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE143nnn/GSE143435/suppl/GSE143435_DeMicheli_D0_FACSatlas_normalizeddata.txt.gz")
 D0_FACSatlas <- D0_FACSatlas %>%
   as.data.frame() %>%
   column_to_rownames('X1') %>%
@@ -12,6 +12,6 @@ D0_FACSatlas <- D0_FACSatlas %>%
   t()
 D0_FACSatlas[1:5, 1:5]
 
-D0_FACSatlasMetadata <- read_text("~/Reference-Matrix-Generation/data/GSE143435/GSE143435.csv")
-sum(colnames(D0_FACSatlas) %in% D0_FACSatlasMetadata$)
+D0_FACSatlasMetadata <- read_tsv("ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE143nnn/GSE143435/suppl/GSE143435_DeMicheli_D0_FACSatlas_metadata.txt.gz")
+sum(colnames(D0_FACSatlas) %in% D0_FACSatlasMetadata$cell_annotation)
 ncol(D0_FACSatlas)
