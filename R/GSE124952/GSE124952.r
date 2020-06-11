c3c06d645015336b600cc54d83400a1378abc72f
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-06-11 09:04:03 -0600

    Errors with creating percent.mt

--- R/GSE124952/GSE124952.r
+++ R/GSE124952/GSE124952.r
@@ -4,7 +4,7 @@ library(patchwork)
 library(clustifyr)
 library(tidyverse)
 
-mat_PFC <- read.csv("ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE124nnn/GSE124952/suppl/GSE124952_expression_matrix.csv.gz")
+mat_PFC <- read_csv("ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE124nnn/GSE124952/suppl/GSE124952_expression_matrix.csv.gz")
 #Error in read.table(file = file, header = header, sep = sep, quote = quote,  : 
 #more columns than column names
 #In addition: Warning message:
@@ -12,8 +12,20 @@ mat_PFC <- read.csv("ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE124nnn/GSE124952/s
 #                  line 1 appears to contain embedded nulls
 mat_PFC <- mat_PFC %>%
   as.data.frame() %>%
-  column_to_rownames('') %>%
+  column_to_rownames('X1') %>%
   as.matrix() %>%
   t() 
 mat_PFC[1:5, 1:5]
-    
+
+meta_PFC <- read_csv("ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE124nnn/GSE124952/suppl/GSE124952_meta_data.csv.gz")
+meta_PFC
+sum(colnames(mat_PFC) %in% meta_PFC$X1)
+ncol(mat_PFC)
+
+#Preprocessing workflow
+PFC <- CreateSeuratObject(counts = mat_PFC, project = "MousePFC", min.cells = 3, min.features = 200)
+PFC
+PFC@meta.data
+PFC@assays$RNA@data <- PFC@assays$RNA@counts
+PFC[["percent.mt"]] <- meta_PFC$percent.mito #Error: Cannot add more or fewer cell meta.data information without values being named with cell names
+head(PFC@meta.data, 5)
