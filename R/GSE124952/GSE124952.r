3552c26ff2adbf19792416c0771d106959a7cc43
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-06-11 08:48:00 -0600

    Added error message

--- R/GSE124952/GSE124952.r
+++ R/GSE124952/GSE124952.r
@@ -5,6 +5,11 @@ library(clustifyr)
 library(tidyverse)
 
 mat_PFC <- read.csv("ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE124nnn/GSE124952/suppl/GSE124952_expression_matrix.csv.gz")
+#Error in read.table(file = file, header = header, sep = sep, quote = quote,  : 
+#more columns than column names
+#In addition: Warning message:
+  #In read.table(file = file, header = header, sep = sep, quote = quote,  :
+#                  line 1 appears to contain embedded nulls
 mat_PFC <- mat_PFC %>%
   as.data.frame() %>%
   column_to_rownames('') %>%
