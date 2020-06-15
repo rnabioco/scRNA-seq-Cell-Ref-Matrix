e5b553b7550ae3b740746725871625213a99e56c
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-06-15 13:57:09 -0600

    Added D7 ref matrix, GSE143435

---
+++ R/GSE143435/D7/GSE143435_D7.r
@@ -0,0 +1,24 @@
+library(dplyr)
+library(Seurat)
+library(patchwork)
+library(clustifyr)
+library(tidyverse)
+
+D7_FACSatlas <- read_tsv("ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE143nnn/GSE143435/suppl/GSE143435_DeMicheli_D7_FACSatlas_normalizeddata.txt.gz")
+D7_FACSatlas <- D7_FACSatlas %>%
+  as.data.frame() %>%
+  column_to_rownames('X1') %>%
+  as.matrix() %>%
+  t()
+D7_FACSatlas[1:5, 1:5]
+
+D7_FACSatlasMetadata <- read_tsv("ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE143nnn/GSE143435/suppl/GSE143435_DeMicheli_D7_FACSatlas_metadata.txt.gz")
+D7_FACSatlasMetadata
+sum(colnames(D7_FACSatlas) %in% D7_FACSatlasMetadata$X1)
+ncol(D7_FACSatlas)
+
+#Reference matrix build
+new_ref_matrix <- average_clusters(mat = D7_FACSatlas, metadata = D7_FACSatlasMetadata$cell_annotation, if_log = TRUE) #Using clustifyr seurat_ref function
+head(new_ref_matrix)
+tail(new_ref_matrix)            
+saveRDS(new_ref_matrix, "GSE143435D7.rds")
