aa3c03029a78759ef13c3775f25c9a7265102692
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-06-16 14:29:33 -0600

    Added hashed reference matrix for GSE147405

--- R/GSE147405/GSE147405.r
+++ R/GSE147405/GSE147405.r
@@ -3,6 +3,7 @@ library(Seurat)
 library(patchwork)
 library(clustifyr)
 library(tidyverse)
+library(digest)
 
 KinaseScreen <- read_csv("ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE147nnn/GSE147405/suppl/GSE147405_OVCA420_TNF_TimeCourse_UMI_matrix.csv.gz")
 KinaseScreen <- KinaseScreen %>%  
@@ -17,6 +18,12 @@ sum(colnames(KinaseScreen) %in% meta_KinaseScreen$CellLine)
 ncol(KinaseScreen)
 
 new_ref_matrix <- average_clusters(mat = KinaseScreen, metadata = meta_KinaseScreen$CellLine, if_log = TRUE)
+new_ref_matrix_hashed <- average_clusters(mat = KinaseScreen, metadata = meta_KinaseScreen$CellLine, if_log = TRUE)
 head(new_ref_matrix)
 tail(new_ref_matrix)
+newcols <- sapply(colnames(new_ref_matrix_hashed), digest, algo = "sha1")
+colnames(new_ref_matrix_hashed) <- newcols
+head(new_ref_matrix_hashed)
+tail(new_ref_matrix_hashed)
+saveRDS(new_ref_matrix_hashed, "GSE147405Hashed.rds")
 saveRDS(new_ref_matrix, "GSE147405.rds")
