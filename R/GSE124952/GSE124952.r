909a0af53d15ed855d873915a29347fe79a3f5db
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-06-16 14:19:15 -0600

    Added hashed refmatrix for GSE124952

--- R/GSE124952/GSE124952.r
+++ R/GSE124952/GSE124952.r
@@ -3,6 +3,7 @@ library(Seurat)
 library(patchwork)
 library(clustifyr)
 library(tidyverse)
+library(digest)
 
 mat_PFC <- read_csv("ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE124nnn/GSE124952/suppl/GSE124952_expression_matrix.csv.gz")
 #Error in read.table(file = file, header = header, sep = sep, quote = quote,  : 
@@ -23,8 +24,14 @@ sum(colnames(mat_PFC) %in% meta_PFC$CellType)
 ncol(mat_PFC)
 
 new_ref_matrix <- average_clusters(mat = mat_PFC, metadata = meta_PFC$CellType, if_log = TRUE)
+new_ref_matrix_hashed <- average_clusters(mat = mat_PFC, metadata = meta_PFC$CellType, if_log = TRUE)
 head(new_ref_matrix)
 tail(new_ref_matrix)
+newcols <- sapply(colnames(new_ref_matrix_hashed), digest, algo = "sha1")
+colnames(new_ref_matrix_hashed) <- newcols
+head(new_ref_matrix_hashed)
+tail(new_ref_matrix_hashed)
+saveRDS(new_ref_matrix_hashed, "GSE124952Hashed.rds")
 saveRDS(new_ref_matrix, "GSE124952.rds")
 
 #Preprocessing workflow
