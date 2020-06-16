063f96c256f4fa2a20e3d4ff926bdacde0b100cc
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-06-16 12:47:00 -0600

    Added hashed rds for GSE129933

--- R/GSE129933/GSE129933.r
+++ R/GSE129933/GSE129933.r
@@ -3,6 +3,7 @@ library(Seurat)
 library(patchwork)
 library(clustifyr)
 library(tidyverse)
+library(digest)
 
 mat_LiverEndothelial <- read_tsv("ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE129nnn/GSE129933/suppl/GSE129933_count_matrix.tsv.gz")
 mat_LiverEndothelial <- mat_LiverEndothelial %>%
@@ -18,6 +19,12 @@ sum(colnames(mat_LiverEndothelial) %in% meta_LiverEndothelial$cluster)
 ncol(mat_LiverEndothelial)
 
 new_ref_matrix <- average_clusters(mat = mat_LiverEndothelial, metadata = meta_LiverEndothelial, if_log = FALSE)
+new_ref_matrix_hashed <- average_clusters(mat = mat_LiverEndothelial, metadata = meta_LiverEndothelial, if_log = FALSE)
 head(new_ref_matrix)
 tail(new_ref_matrix)
+newcols <- sapply(colnames(new_ref_matrix_hashed), digest, algo = "sha1")
+colnames(new_ref_matrix_hashed) <- newcols
+head(new_ref_matrix_hashed)
+tail(new_ref_matrix_hashed)
+saveRDS(new_ref_matrix_hashed, "GSE129933Hashed.rds")
 saveRDS(new_ref_matrix, "GSE129933.rds")
