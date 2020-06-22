f438d09d89eac294352ccde214d665c6d9bae339
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-06-22 09:58:37 -0600

    Changed matrices to dataframes for concatenation

--- R/homoSapiens/humanAtlas.r
+++ R/homoSapiens/humanAtlas.r
@@ -6,10 +6,17 @@ library(tidyverse)
 library(digest)
 
 GSE129933Filename <- file.choose()
-GSE129933 <- readRDS(GSE129933Filename)
+GSE129933Matrix <- readRDS(GSE129933Filename)
 
 GSE137710Filename <- file.choose()
 GSE137710 <- readRDS(GSE137710Filename)
 
 GSE147405Filename <- file.choose()
-GSE147405 <- readRDS(GSE147405Filename)
+GSE147405Matrix <- readRDS(GSE147405Filename)
+
+GSE129933 <- as.data.frame(GSE129933Matrix)
+GSE147405 <- as.data.frame(GSE147405Matrix)
+
+rm(GSE129933Matrix)
+rm(GSE147405Matrix)
+
