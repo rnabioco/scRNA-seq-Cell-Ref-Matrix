3cecf862c411b11b4ee2f7ad7d3c6d0f23b313e3
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-06-23 09:25:22 -0600

    Added D2, D5, and D7 ref matrices to atlas

--- R/musMusculus/mouseAtlas.r
+++ R/musMusculus/mouseAtlas.r
@@ -11,8 +11,17 @@ GSE113049 <- readRDS(GSE113049Filename)
 GSE124952Filename <- file.choose()
 GSE124952 <- readRDS(GSE124952Filename)
 
-GSE143405Filename <- file.choose()
-GSE143405 <- readRDS(GSE143405Filename)
-
 GSE137710Filename <- file.choose()
 GSE137710 <- readRDS(GSE137710Filename)
+
+GSE143405D0Filename <- file.choose()
+GSE143405_D0 <- readRDS(GSE143405D0Filename)
+
+GSE143435D2Filename <- file.choose()
+GSE143435_D2 <- readRDS(GSE143435D2Filename)
+
+GSE143435D5Filename <- file.choose()
+GSE143435_D5 <- readRDS(GSE143435D5Filename)
+
+GSE143435D7Filename <- file.choose()
+GSE143435_D7 <- readRDS(GSE143435D7Filename)
