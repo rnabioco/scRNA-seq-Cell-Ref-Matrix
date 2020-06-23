869cc22d2ba44a451f9b0c1140662d9a1ea3fe25
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-06-23 13:12:50 -0600

    Added melanoma file call in atlas

--- R/homoSapiens/humanAtlas.r
+++ R/homoSapiens/humanAtlas.r
@@ -8,8 +8,11 @@ library(digest)
 GSE129933Filename <- file.choose()
 GSE129933Matrix <- readRDS(GSE129933Filename)
 
-GSE137710Filename <- file.choose()
-GSE137710 <- readRDS(GSE137710Filename)
+GSE137710FilenameMelanoma <- file.choose()
+GSE137710Melanoma <- readRDS(GSE137710FilenameMelanoma)
+
+GSE137710FilenameSpleen <- file.choose()
+GSE137710Spleen <- readRDS(GSE137710FilenameSpleen)
 
 GSE147405Filename <- file.choose()
 GSE147405Matrix <- readRDS(GSE147405Filename)
@@ -20,4 +23,4 @@ GSE147405 <- as.data.frame(GSE147405Matrix)
 rm(GSE129933Matrix)
 rm(GSE147405Matrix)
 
-bind_rows(GSE129933, GSE137710, GSE147405, .id = NULL)
+bind_rows(GSE129933, GSE137710Melanoma, GSE137710Spleen, GSE147405, .id = NULL)
