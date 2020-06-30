1fc7ee4c161ecc6ee3783599ec2a28778f425b50
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-06-30 09:51:39 -0600

    Changed functionlogic

--- R/homoSapiens/humanAtlas.r
+++ R/homoSapiens/humanAtlas.r
@@ -53,25 +53,29 @@ GSE137710Spleen <- GSE137710Spleen[common, ]
 GSE147405 <- GSE147405[common, ]
 GSE147405 <- as.data.frame(GSE147405)
 
-appendGenes <- function(humanGenes, newGSEFile)
+appendGenes <- function(humanGenesVector, GSEMatrix)
 {
-  rownamesNewGSEFile <- rownames(newGSEFile)
+  rownamesGSEMatrix <- rownames(GSEMartix) #Get rownames from GSEMatrix (new GSE file)
   
-  rowCountHumanGenes <- nrow(humanGenes)
-  rowCountNewGSEFile <- nrow(newGSEFile)
+  rowCountHumanGenes <- nrow(humanGenesVector) #Calculate number of rows from list of full human genes
+  rowCountNewGSEFile <- nrow(GSEMatrix) #Calculate number of rows of GSE matrix
   
-  for (i in rowCountNewGSEFile)
-  {
-    searchGene <- rownamesNewGSEFile[i]
-    for (j in rowCountHumanGenes)
-    {
-      if (searchGene != humanGenes[j,1])
-      {
-        newGeneArray <- searchGene
-      }
-    }
-  }
-  return(newGeneArray)
+  missing_rows <- setdiff(fullHumanGenes[1, ], rownamesNewGSEMatrix) #Use setdiff function to figure out rows which are different/missing from GSE matrix
+  missing_rows #Display missing rows
+  
+  zeroExpressionMatrix <- matrix(0, nrow = length(missing_rows), ncol = ncol()) #Create a placeholder matrix with zeroes and missing_rows length as row length
+  zeroExpressionMatrix[1:3, 1:3] #Check first three entries of zeroExpressionMatrix
+  dim(zeroExpressionMatrix) #Check dimensions of matrix
+  
+  rownames(zeroExpressionMatrix) <- missing_rows #Assign row names
+  colnames(zeroExpressionMatrix) <- colnames(GSEMatrix) #Assign column names
+  zeroExpressionMatrix[1:3, 1:3] #Check col and row names assigned correctly
+  
+  fullMatrix <- rbind(GSEMatrix, zeroExpressionMatrix) #Bind GSEMatrix and zeroExpressionMatrix together
+  dim(fullMatrix) #Check dimensions of fullMatrix
+  
+  fullMatrix <- fullMatrix[]
+  
 }
 
 GSE129933NewGeneNames <- appendGenes(humanGenes = fullHumanGenes, newGSEFile = GSE129933)
