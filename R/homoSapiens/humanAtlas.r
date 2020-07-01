7f8fc5c111ba49488b14643ae7420a2f8cc3fdb4
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-07-01 13:02:05 -0600

    Got rid of unecessary lines

--- R/homoSapiens/humanAtlas.r
+++ R/homoSapiens/humanAtlas.r
@@ -37,24 +37,6 @@ rm(GSE137710FilenameMelanoma)
 rm(GSE137710FilenameSpleen)
 rm(humanGenesFile)
 
-common <- Reduce(intersect, list(rownames(GSE129933), rownames(GSE137710Melanoma), rownames(GSE137710Spleen), rownames(GSE147405)))
-GSE129933[common,] # give you common rows in data frame 1  
-GSE137710Spleen[common,] # give you common rows in data frame 2
-GSE137710Melanoma[common,]
-GSE147405[common,]
-head(common)
-
-#GSE129933 <- subset(GSE129933, rownames(GSE129933) %in% c(common))
-#GSE137710Melanoma <- subset(GSE137710Melanoma, rownames(GSE137710Melanoma) %in% c(common))
-#GSE137710Spleen <- subset(GSE137710Spleen, rownames(GSE137710Spleen) %in% c(common))
-#GSE147405 <- subset(GSE147405, rownames(GSE147405) %in% c(common))
-
-GSE129933 <- GSE129933[common, ]
-GSE137710Melanoma <- GSE137710Melanoma[common, ]
-GSE137710Spleen <- GSE137710Spleen[common, ]
-GSE147405 <- GSE147405[common, ]
-GSE147405 <- as.data.frame(GSE147405)
-
 appendGenes <- function(humanGenesVector, GSEMatrix)
 {
   rownamesGSEMatrix <- rownames(GSEMatrix) #Get rownames from GSEMatrix (new GSE file)
