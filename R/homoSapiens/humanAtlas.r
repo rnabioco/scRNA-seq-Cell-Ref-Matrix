055bfb277db5c8beb90e2956d621cfabafb501f7
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-07-08 08:58:43 -0600

    Deleted checkRawCounts from mouseAtlas and humanAtlas. Function moved to check.r in utils folder

--- R/homoSapiens/humanAtlas.r
+++ R/homoSapiens/humanAtlas.r
@@ -58,33 +58,6 @@ appendGenes <- function(humanGenesVector, GSEMatrix)
   return(fullMatrix) #Return fullMatrix
 }
 
-checkRawCounts <- function(GSEMatrix, max_log_value = 50)
-{
-  if (is.integer(GSEMatrix))
-  {
-    return("raw counts")
-  }
-  else if (is.double(GSEMatrix))
-  {
-    if(max(x) > max_log_val)
-    {
-      return("normalized")
-    } 
-    else if (min(x) < 0) 
-    {
-      stop("negative values detected, likely scaled data")
-    } 
-    else 
-    {
-      return("log-normalized")
-    }
-  }
-  else 
-  {
-    stop("unknown matrix format: ", typeof(x))
-  }
-}
-
 source("~/Reference-Matrix-Generation/R/utils/check.r")
 
 checkRawCounts(GSE129933Matrix)
--- R/musMusculus/mouseAtlas.r
+++ R/musMusculus/mouseAtlas.r
@@ -52,33 +52,6 @@ appendGenes <- function(mouseGenesVector, GSEMatrix)
   return(fullMatrix) #Return fullMatrix
 }
 
-checkRawCounts <- function(GSEMatrix, max_log_value = 50)
-{
-  if (is.integer(GSEMatrix))
-  {
-    return("raw counts")
-  }
-  else if (is.double(GSEMatrix))
-  {
-    if(max(x) > max_log_val)
-    {
-      return("normalized")
-    } 
-    else if (min(x) < 0) 
-    {
-      stop("negative values detected, likely scaled data")
-    } 
-    else 
-    {
-      return("log-normalized")
-    }
-  }
-  else 
-  {
-    stop("unknown matrix format: ", typeof(x))
-  }
-}
-
 source("~/Reference-Matrix-Generation/R/utils/check.r")
 
 checkRawCounts(GSE113049, 50)
