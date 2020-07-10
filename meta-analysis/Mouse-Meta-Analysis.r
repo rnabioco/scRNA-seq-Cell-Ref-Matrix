c369b91d9043b7feb6f0fddd297a3c2b342838f3
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-07-10 10:48:14 -0600

    Corrected function name to DimHeatmap

--- meta-analysis/Mouse-Meta-Analysis.r
+++ meta-analysis/Mouse-Meta-Analysis.r
@@ -20,7 +20,7 @@ head(mouseMetaAnalysis@meta.data, 5)
 VlnPlot(mouseMetaAnalysis, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
 plot1 <- FeatureScatter(mouseMetaAnalysis, feature1 = "nCount_RNA", feature2 = "percent.mt")
 plot2 <- FeatureScatter(mouseMetaAnalysis, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
-CombinePlots(plots = list(plot1, plot2))
+plot1 + plot2
 
 #Find Variable Features for PCA
 mouseMetaAnalysis <- FindVariableFeatures(mouseMetaAnalysis, selection.method = "mean.var.plot", nfeatures = 2000)
@@ -39,5 +39,5 @@ mouseMetaAnalysis <- RunPCA(mouseMetaAnalysis,
 print(mouseMetaAnalysis[["pca"]], dims = 1:5, nfeatures = 5)
 VizDimLoadings(mouseMetaAnalysis, dims = 1:2, reduction = "pca")
 DimPlot(mouseMetaAnalysis, reduction = "pca")
-DimHeatMap(mouseMetaAnalysis, dims = 1, cells = 500, balanced = TRUE)
-DimHeatMap(mouseMetaAnalysis, dims = 1:15, cells = 500, balanced = TRUE)
+DimHeatmap(mouseMetaAnalysis, dims = 1, cells = 500, balanced = TRUE)
+DimHeatmap(mouseMetaAnalysis, dims = 1:15, cells = 500, balanced = TRUE)
