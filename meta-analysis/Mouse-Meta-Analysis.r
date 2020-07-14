c6f79fb7dda594fcae25ed8db5e89871363db0fb
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-07-13 18:52:01 -0600

    Refined UMAP code by calculating number of clusters

--- meta-analysis/Mouse-Meta-Analysis.r
+++ meta-analysis/Mouse-Meta-Analysis.r
@@ -68,7 +68,7 @@ cluster1.markers <- FindMarkers(mouseMetaAnalysis, ident.1 = 0, logfc.threshold 
 top10 <- mouseMetaAnalysis.markers %>% group_by(cluster) %>% top_n(n = 10, wt = avg_logFC) #Create top 10 markers for each cluster
 DoHeatmap(pbmc, features = top10$gene) + NoLegend() #Create heat map of top 10 markers
 
-#Assign cell types
+#Assign cell types/Annotate UMAP
 new.cluster.ids <- colnames(mouseAtlas)
 names(new.cluster.ids) <- levels(mouseMetaAnalysis)
 mouseMetaAnalysis <- RenameIdents(mouseMetaAnalysis, new.cluster.ids)
