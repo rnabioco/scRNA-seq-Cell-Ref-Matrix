13e04ccee9f365c140003b18b1e8a3529b112e90
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-07-22 09:28:39 -0600

    Added initial ref matrix for GSE115746

--- R/musMusculus/GSE115746/GSE115746.r
+++ R/musMusculus/GSE115746/GSE115746.r
@@ -7,13 +7,16 @@ library(digest)
 library(here)
 
 proj_dir <- here()
+GSE115746 <- file.path(proj_dir, "Reference-Matrix-Generation", "data", "GSE115746", "GSE115746_cells_exon_counts.csv")
 GSE115746Meta <- file.path(proj_dir, "Reference-Matrix-Generation", "data", "GSE115746", "GSE115746_complete_metadata_28706-cells.csv")
 
-mat_GSE115746 <- read.csv("ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE115nnn/GSE115746/suppl/GSE115746_cells_exon_counts.csv.gz", row.names = 1, check.names = F)
+mat_GSE115746 <- read_csv(GSE115746) %>% column_to_rownames("X1")
 mat_GSE115746 <- as.matrix(mat_GSE115746)
 mat_GSE115746[1:5, 1:5]
 
-meta_GSE115746 <- read.csv(GSE115746Meta)
+meta_GSE115746 <- read_csv(GSE115746Meta, skip = 8)
+meta_GSE115746 <- as.data.frame(meta_GSE115746)
+names(meta_GSE115746) <- colnames(read_csv(GSE115746Meta, n_max = 1))
 meta_GSE115746
 ncol(mat_GSE115746)
 
