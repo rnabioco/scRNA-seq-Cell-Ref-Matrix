6d4ced2af9528545803ad1a4ca202a5488cc0c4e
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-07-27 12:52:25 -0600

    Added PCA and UMAP code

--- R/musMusculus/AtlasComparison.r
+++ R/musMusculus/AtlasComparison.r
@@ -22,3 +22,29 @@ plot2 <- FeatureScatter(SeuratLung, feature1 = "nCount_RNA", feature2 = "nFeatur
 plot1 + plot2
 
 SeuratLung <- NormalizeData(SeuratLung)
+SeuratLung <- FindVariableFeatures(SeuratLung, selection.method = "vst", nfeatures = 2000)
+
+# Identify the 10 most highly variable genes
+top10 <- head(VariableFeatures(SeuratLung), 10)
+
+# plot variable features with and without labels
+plot1 <- VariableFeaturePlot(SeuratLung)
+plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+plot1 + plot2
+
+all.genes <- rownames(SeuratLung)
+SeuratLung <- ScaleData(SeuratLung, features = all.genes)
+SeuratLung <- RunPCA(SeuratLung, features = VariableFeatures(object = SeuratLung))
+print(SeuratLung[["pca"]], dims = 1:5, nfeatures = 5)
+VizDimLoadings(SeuratLung, dims = 1:2, reduction = "pca")
+DimPlot(SeuratLung, reduction = "pca")
+DimHeatmap(SeuratLung, dims = 1, cells = 500, balanced = TRUE)
+DimHeatmap(SeuratLung, dims = 1:15, cells = 500, balanced = TRUE)
+
+ElbowPlot(SeuratLung)
+SeuratLung <- FindNeighbors(SeuratLung, dims = 1:10)
+SeuratLung <- FindClusters(SeuratLung, resolution = 0.5)
+head(Idents(SeuratLung), 5)
+
+SeuratLung <- RunUMAP(SeuratLung, dims = 1:10)
+DimPlot(SeuratLung, reduction = "umap")
